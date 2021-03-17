efc19eb57756a0d11218cd13a6a165015d119e5c
Dhanush669 <dhanushrukmangadhan@gmail.com>
2021-03-17 13:15:20 +0530

    Initial Commit

--- app/src/main/java/com/example/shortcutdemo/MainActivity.java
+++ app/src/main/java/com/example/shortcutdemo/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.shortcutdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ShortcutInfo;
 import android.content.pm.ShortcutManager;
 import android.graphics.drawable.Icon;
 import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,6 +19,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     ShortcutManager shortcutManager;
     List<ShortcutInfo> shortcuts;
+    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,5 +45,10 @@ public class MainActivity extends AppCompatActivity {
         shortcuts.add(photo_shortcut);
         shortcuts.add(video_shortcut);
         shortcutManager.setDynamicShortcuts(shortcuts);
+    }
+
+    public void openVideo(View view) {
+        Intent intent=new Intent(MainActivity.this,VideoActivity.class);
+        startActivity(intent);
     }
 }
